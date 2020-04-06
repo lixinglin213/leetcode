2162a4f2ab983c82651683df5c254547abaaf50d
李星霖 <lixinglin@thunisoft.com>
2020-04-06 14:59:51 +0800

    新增冒泡排序

---
+++ algorithm/src/sort/Bubble.java
@@ -0,0 +1,35 @@
+package sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ *
+ * @author lixinglin
+ * @date 2020/4/6 2:50 下午
+ * @since jdk 1.8
+ */
+public class Bubble {
+
+    public static void main(String[] args) {
+        int[] nums = {4, 2, 1, 1, 6, 3};
+        int[] sort = Bubble.sort(nums);
+        System.out.print(Arrays.toString(sort));
+    }
+
+    public static int[] sort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > i; j--) {
+
+                if (nums[j] < nums[j - 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = tmp;
+                }
+
+            }
+        }
+        return nums;
+    }
+
+}
