cf37ea7cde860b87ce9f38dcad63a1a92b5f88c6
李星霖 <lixinglin@thunisoft.com>
2020-04-19 12:57:35 +0800

    提交treenode的构造

---
+++ algorithm/src/utils/TreeNode.java
@@ -0,0 +1,18 @@
+package utils;
+
+/**
+ * @author lixinglin
+ * @date 2020/4/8 9:24 上午
+ * @since jdk 1.8
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
